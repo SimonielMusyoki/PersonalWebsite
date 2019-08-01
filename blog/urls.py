39cf9887125f209c865fc16d705241f3cbf36697
SimonielMusyoki <musyoki.mtk3@gmail.com>
2019-08-01 11:40:39 +0300

    Blog URLs

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
+from .views import ProjectDeleteView,ProjectUpdateView,Contact,MessagesListView,ProjectListView,ProjectDetailView,ProjectCreateView, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
+
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
@@ -9,5 +10,12 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('projects/new/', ProjectCreateView.as_view(), name='project-create'),
+    path('project/<int:pk>/update', ProjectUpdateView.as_view(), name='project-update'),
+    path('projects/', ProjectListView.as_view(), name='projects'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project-delete'),
     path('about/', views.about, name='blog-about'),
+    path('contact/', Contact.as_view(), name='blog-contact'),
+    path('messages/', MessagesListView.as_view(), name='messages'),
 ]
