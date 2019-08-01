c3ce84f3da60e1744a0c9665adcb5ac15cf6be8e
SimonielMusyoki <musyoki.mtk3@gmail.com>
2019-08-01 11:40:06 +0300

    Registered models

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Post
+from .models import Messages,Post, Projects
 
 admin.site.register(Post)
+admin.site.register(Messages)
+admin.site.register(Projects)
+
 
 
 # Register your models here.
